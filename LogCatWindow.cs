953cc5ecb48f29fbed38f16a6ff7f5f231aff772
Rokas Brazdzionis <rokas.brazdzionis@gmail.com>
2014-06-08 15:27:54 +0300

    Windowed cmd fix on win os. Minor refactories.

--- LogCatWindow.cs
+++ LogCatWindow.cs
@@ -18,7 +18,7 @@ public class LogCatWindow : EditorWindow
     private bool filterOnlyErrors;
     private bool filterOnlyWarnings;
     private bool filterOnlyDebugs;
-    private string searchString = "";
+    private string filterByString = "";
 
     // Android adb logcat proccess
     private Process proccess;
@@ -34,7 +34,7 @@ public class LogCatWindow : EditorWindow
     [MenuItem("Window/LogCat - Android Logger")]
     public static void ShowWindow()
     {
-        //Show existing window instance. If one doesn't exist, make one.
+        // Show existing window instance. If one doesn't exist, make one.
         EditorWindow.GetWindow(typeof(LogCatWindow), false, "Logcat");
     }
     
@@ -46,7 +46,7 @@ public class LogCatWindow : EditorWindow
         lock (logsList)
         {
             // Filter
-            filteredList = logsList.Where(log => (searchString.Length <= 2 || log.Message.ToLower().Contains(searchString.ToLower())) &&
+            filteredList = logsList.Where(log => (filterByString.Length <= 2 || log.Message.ToLower().Contains(filterByString.ToLower())) &&
                 ((!filterOnlyErrors && !filterOnlyWarnings && !filterOnlyDebugs) 
                 || filterOnlyErrors && log.Type == 'E' 
                 || filterOnlyWarnings && log.Type == 'W' 
@@ -63,7 +63,7 @@ public class LogCatWindow : EditorWindow
         if (GUILayout.Button("Start logging", GUILayout.Height(20), GUILayout.Width(100)))
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = false;
+            startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
             startInfo.RedirectStandardError = true;
@@ -75,11 +75,11 @@ public class LogCatWindow : EditorWindow
             
             proccess.ErrorDataReceived += (sender, errorLine) => { 
                 if (errorLine.Data != null && errorLine.Data.Length > 2)
-                    addToLogList(new LogCatLog(errorLine.Data)); 
+                    AddLog(new LogCatLog(errorLine.Data)); 
             };
             proccess.OutputDataReceived += (sender, outputLine) => { 
                 if (outputLine.Data != null && outputLine.Data.Length > 2)
-                    addToLogList(new LogCatLog(outputLine.Data)); 
+                    (new LogCatLog(outputLine.Data)); 
             };
             proccess.BeginErrorReadLine();
             proccess.BeginOutputReadLine();
@@ -110,12 +110,12 @@ public class LogCatWindow : EditorWindow
         filterOnlyWarnings = GUILayout.Toggle(filterOnlyWarnings, "Warnings", "Button", GUILayout.Width(80));
         filterOnlyDebugs = GUILayout.Toggle(filterOnlyDebugs, "Debugs", "Button", GUILayout.Width(80));
 
-        searchString = GUILayout.TextField(searchString, GUILayout.Height(20));
+        filterByString = GUILayout.TextField(filterByString, GUILayout.Height(20));
 
         GUILayout.EndHorizontal(); 
 
         GUIStyle lineStyle = new GUIStyle();
-        lineStyle.normal.background = MakeTex(600, 1, new Color(1.0f, 1.0f, 1.0f, 0.1f));
+        lineStyle.normal.background = MakeTexture(600, 1, new Color(1.0f, 1.0f, 1.0f, 0.1f));
 
         scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Height(Screen.height - 45));
         
@@ -126,8 +126,8 @@ public class LogCatWindow : EditorWindow
 
         for (int i = fromIndex; i < filteredList.Count; i++)
         {
-            LogCatLog log = filteredList [i];
-            GUI.backgroundColor = log.getBgColor();
+            LogCatLog log = filteredList[i];
+            GUI.backgroundColor = log.GetBgColor();
             GUILayout.BeginHorizontal(lineStyle);
             GUILayout.Label(log.CreationDate + " | " + log.Message);
             GUILayout.EndHorizontal(); 
@@ -136,53 +136,7 @@ public class LogCatWindow : EditorWindow
         GUILayout.EndScrollView();
     }
 
-    private class LogCatLog
-    {
-        public LogCatLog(string data)
-        {
-            Type = data [0];
-            Message = data.Substring(2);
-            CreationDate = DateTime.Now.ToString("H:mm:ss");
-        }
-
-        public string CreationDate
-        {
-            get;
-            set;
-        }
-
-        public char Type
-        {
-            get;
-            set;
-        }
-
-        public string Message
-        {
-            get;
-            set;
-        }
-
-        public Color getBgColor()
-        {
-            switch (Type)
-            {
-                case 'W':
-                    return Color.yellow;
-
-                case 'E':
-                    return Color.red;
-
-                case 'D':
-                    return Color.blue;
-
-                default:
-                    return Color.grey;
-            }
-        }
-    }
-
-    private Texture2D MakeTex(int width, int height, Color col)
+    private Texture2D MakeTexture(int width, int height, Color col)
     {
         Color[] pix = new Color[width * height];
         
@@ -196,7 +150,7 @@ public class LogCatWindow : EditorWindow
         return result;
     }
 
-    private void addToLogList(LogCatLog log)
+    private void AddLog(LogCatLog log)
     {
         lock (logsList)
         {
@@ -204,6 +158,58 @@ public class LogCatWindow : EditorWindow
                 logsList.RemoveRange(0, logsList.Count - memoryLimit + 1);
 
             logsList.Add(log);
+        }
+    }
+
+    private class LogCatLog
+    {
+        public LogCatLog(string data)
+        {
+            // First char indicates error type:
+            // W - warning
+            // E - error
+            // D - debug
+            // V - verbose
+            Type = data[0];
+
+            Message = data.Substring(2);
+            CreationDate = DateTime.Now.ToString("H:mm:ss");
+        }
+        
+        public string CreationDate
+        {
+            get;
+            set;
+        }
+        
+        public char Type
+        {
+            get;
+            set;
+        }
+        
+        public string Message
+        {
+            get;
+            set;
+        }
+        
+        public Color GetBgColor()
+        {
+            switch (Type)
+            {
+                case 'W':
+                    return Color.yellow;
+                    
+                case 'E':
+                    return Color.red;
+                    
+                case 'D':
+                    return Color.blue;
+                    
+                default:
+                    return Color.grey;
+            }
         }
     }
 }
